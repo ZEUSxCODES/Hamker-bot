3a70f028ba959423c52d16f61e0cbbda15223a44
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-10 10:04:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-api_id = "YOUR_API_ID"
-api_hash = "YOUR_API_HASH"
-bot_token = "YOUR_BOT_TOKEN"
-START_PIC = "https://te.legra.ph/file/1f2ac2fe8cdf202799847.jpg"
+api_id = "YOUR_API_ID"  # Replace with your Telegram API ID
+api_hash = "YOUR_API_HASH"  # Replace with your Telegram API hash
+bot_token = "YOUR_BOT_TOKEN"  # Replace with your Telegram bot token
+START_PIC = "https://te.legra.ph/file/1f2ac2fe8cdf202799847.jpg"  # URL for bot start photo
+SOURCE_VID = "https://te.legra.ph/file/fecf4e578f159374f33c4.mp4"  # URL for source video
+BOT_USERNAME = "Bot_Username"  # Replace with your bot's Telegram username
