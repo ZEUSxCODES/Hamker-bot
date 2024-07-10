83af28d2392e5daf3eb8af681c17d134dd85edb1
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-10 10:40:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,61 @@
-from pyrogram import Client
-import config
-from plugins import commands, pmfilter
+import asyncio
+import logging
+import logging.config
+from config import Config
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from pyrogram.enums import ParseMode
+from pyrogram.errors import FloodWait
+from database import db
 
-# Initialize the bot
-app = Client(
-    "bot",
-    api_id=config.api_id,
-    api_hash=config.api_hash,
-    bot_token=config.bot_token
-)
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
-# Register command handlers
-commands.register_commands(app)
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            Config.BOT_SESSION,
+            api_hash=Config.API_HASH,
+            api_id=Config.API_ID,
+            plugins={
+                "root": "plugins"
+            },
+            workers=50,
+            bot_token=Config.BOT_TOKEN
+        )
+        self.log = logging
 
-# Register PM filter
-pmfilter.register_pmfilter(app)
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        logging.info(f"{me.first_name} with for pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
+        self.id = me.id
+        self.username = me.username
+        self.first_name = me.first_name
+        self.set_parse_mode(ParseMode.DEFAULT)
+        text = "**Hamker Bot restarted!**"
+        logging.info(text)
+        success = failed = 0
+        users = await db.get_all_users()
+        async for user in users:
+            chat_id = user['id']
+            try:
+                await self.send_message(chat_id, text)
+                success += 1
+            except FloodWait as e:
+                await asyncio.sleep(e.value + 1)
+                await self.send_message(chat_id, text)
+                success += 1
+            except Exception:
+                failed += 1
+        if (success + failed) != 0:
+            logging.info(f"Restart message status - "
+                         f"success: {success}, "
+                         f"failed: {failed}")
 
-if __name__ == "__main__":
-    # Start the bot
-    app.run()
+    async def stop(self, *args):
+        msg = f"@{self.username} stopped. Bye."
+        await super().stop()
+        logging.info(msg)
+
