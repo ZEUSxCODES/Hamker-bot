b37ddbaafd3ce3d3658671e07535bfde29c5e2f6
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-10 10:03:29 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,20 @@
+# translation.py
+
+START_TXT = "🥀 ᴛʜɪs ɪs ˹ᴛʜᴇ ʜᴧᴍᴋᴇꝛ ʙσᴛ˼,\nᴛʜᴇ ᴍᴏsᴛ ᴜsᴇʟᴇss ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ ᴇᴠᴇʀ ᴍᴀᴅᴇ, ᴀʟɪᴠᴇ sɪɴᴄᴇ {uptime_str}."
+HELP_TXT = """
+Here is the help menu of the bot:
+
+/afk : Mark yourself as AFK (away from keyboard)
+/alive : Shows the alive status of the bot
+/echo : Sends the given text
+/figlet : Generates a figlet of given name
+/img : Shows information about the replied image
+/phlogo : Generates a Pornhub logo of the given text
+/ping : Shows the ping latency and system stats of the bot
+/q : Makes a quote of the replied message
+/shorturl : Shortens the given URL
+/start : Starts the bot
+/sname : Decodes the name of the replied user with Unicode
+/webss : Captures a screenshot of the given site
+"""
+ABOUT_TXT = "This is a multi-feature Telegram bot created using Pyrogram."
